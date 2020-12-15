68dafcee5bc2f2e9c77ceaea7bc2e0b7e6951504
Axel <meloniaxel@yahoo.fr>
2020-12-15 12:48:30 +0100

    Start to looking at how implementing the specific world recognition

--- src/app/service/web-speech-api.service.ts
+++ src/app/service/web-speech-api.service.ts
@@ -8,41 +8,29 @@ declare var webkitSpeechRecognition: any;
 })
 export class VoiceRecognitionService {
 
- recognition =  new webkitSpeechRecognition();
-  isStoppedSpeechRecog = false;
+  recognition =  new webkitSpeechRecognition();
   public text = '';
   tempWords;
 
   constructor() { }
 
-  init() {
-
+  init(){
     this.recognition.interimResults = true;
     this.recognition.lang = 'fr-FR';
 
     this.recognition.addEventListener('result', (e) => {
-      const transcript = Array.from(e.results)
+      /*const transcript = Array.from(e.results)
         .map((result) => result[0])
         .map((result) => result.transcript)
         .join('');
-      this.tempWords = transcript;
-      console.log(transcript);
+      this.tempWords = transcript;*/
+      console.log(e.results[0][0].transcript);
     });
   }
 
   start() {
-    this.isStoppedSpeechRecog = false;
     this.recognition.start();
     console.log("Speech recognition started")
-    this.recognition.addEventListener('end', (condition) => {
-      if (this.isStoppedSpeechRecog) {
-        this.recognition.stop();
-        console.log("End speech recognition")
-      } else {
-        this.wordConcat()
-        this.recognition.start();
-      }
-    });
   }
 
   wordConcat() {
